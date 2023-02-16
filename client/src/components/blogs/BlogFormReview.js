e76c74807f642385a8dff2e9b7272c65d358f0e0
Natália Barros <natalia.vieiramonteiro70@gmail.com>
2023-02-16 15:57:53 -0300

    upload file

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state={file:null};
   renderFields() {
     const { formValues } = this.props;
 
@@ -46,13 +47,16 @@ class BlogFormReview extends Component {
 
     submitBlog(formValues, history);
   }
-
+ onFileChange(event){
+  this.setState({file:event.target.files[0]});
+ }
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
-
+          <h5> Add an image</h5>
+          <input onChange={this.onFileChange.bind(this)}type="file" accept="image/*"/>
         {this.renderButtons()}
       </form>
     );
